66d2f79836af42dda711877eae1d9acc22c9c574
RacyFiend <demonperechemidi@gmail.com>
2025-10-30 13:54:46 +0300

    Deleted Storyboard.main, deleted in info.plist info about storyboard. Corrected SceneDelegate and AppDelegate for app without Storyboard

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,18 +1,14 @@
-//
-//  AppDelegate.swift
-//  Tracker
-//
-//  Created by Дмитрий Перчемиди on 26.10.2025.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
